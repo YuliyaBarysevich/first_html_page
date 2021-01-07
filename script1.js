d95f3285c0fafa5ed8904a3ff9b0d794a93ac01d
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-01-07 12:13:46 -0800

    put all my functions in one js file

--- script1.js
+++ script1.js
@@ -1,3 +1,34 @@
+
+function askUserName() {
 let userName = prompt('Please Enter Your Name:');
 
 document.write('Welcome, ' + userName + '!');
+
+};
+
+function gettingNewsLetters() {
+   let answer = prompt('Do you want to sign up for newsletters? Please enter YES or NO'); 
+
+   if (answer == 'YES' || answer == 'yes'){
+    prompt("We need your email :)")
+     } else if (answer == 'NO' || answer == 'no'){
+    alert('Thank you for your answer!')
+      }else {
+    alert('Please type YES or NO')
+      }
+};
+
+
+function workingHours() {
+let today = new Date();
+let hourNow = today.getHours();
+let workHours;
+
+if (hourNow >= 9 || hourNow < 18) {
+    workHours = 'We are open! Contact Us.'
+} else {
+    workHours = 'Sorry! We are closed now. Contact Us Tomorrow.'
+}
+
+document.write('<h3>' + workHours + '</h3>');
+};
--- script3.js
+++ script3.js
@@ -1,11 +1,11 @@
-let today = new Date();
-let hourNow = today.getHours();
-let workHours;
+// let today = new Date();
+// let hourNow = today.getHours();
+// let workHours;
 
-if (hourNow >= 9 || hourNow < 18) {
-    workHours = 'We are open! Contact Us.'
-} else {
-    workHours = 'Sorry! We are closed now. Contact Us Tomorrow.'
-}
+// if (hourNow >= 9 || hourNow < 18) {
+//     workHours = 'We are open! Contact Us.'
+// } else {
+//     workHours = 'Sorry! We are closed now. Contact Us Tomorrow.'
+// }
 
-document.write('<h3>' + workHours + '</h3>');
+// document.write('<h3>' + workHours + '</h3>');
